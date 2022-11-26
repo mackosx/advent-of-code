162b05a1b0cda7c8cdf3a4cda4bbd15ccdb9b04d
Mackenzie Salloum <mackenziesalloum@gmail.com>
2022-11-26 09:25:35 -0800

    Day 2 puzzle 2

--- 2021/day2.py
+++ 2021/day2.py
@@ -21,5 +21,26 @@ parsed_input = utils.parse_file(
     os.path.join(os.path.dirname(__file__), "day2_input.txt"), as_int=False
 )
 
+
+def puzzle_2(input):
+    position = 0
+    depth = 0
+    aim = 0
+    for instruction in input:
+        direction, amount = instruction.split(" ")
+        amount = int(amount)
+        if direction == "forward":
+            position += amount
+            depth += aim * amount
+        elif direction == "up":
+            aim -= amount
+        elif direction == "down":
+            aim += amount
+    return position * depth
+
+
 result = puzzle_1(parsed_input)
+print(result)
+
+result = puzzle_2(parsed_input)
 print(result)
