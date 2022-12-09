6d5166444513039f05283a6b86c4fcafcb04d3d6
Mackenzie Salloum <mackenziesalloum@gmail.com>
2022-12-08 23:30:32 -0800

    Part 2 with same logic

--- 2022/day8.go
+++ 2022/day8.go
@@ -16,18 +16,21 @@ func isVisible(i int, j int, trees [][]int) bool {
 	if i == 0 || j == 0 {
 		return true
 	}
+
 	for a := i + 1; a < len(trees[i]); a++ {
 		if trees[a][j] >= tree {
 			visible_right = false
 			break
 		}
 	}
+
 	for b := i - 1; b >= 0; b-- {
 		if trees[b][j] >= tree {
 			visible_left = false
 			break
 		}
 	}
+
 	for c := j + 1; c < len(trees); c++ {
 		if trees[i][c] >= tree {
 			visible_bottom = false
@@ -45,7 +48,7 @@ func isVisible(i int, j int, trees [][]int) bool {
 	return visible_bottom || visible_left || visible_top || visible_right
 }
 
-func puzzle1(input string) {
+func parseTrees(input string) [][]int {
 	lines := strings.Split(input, "\n")
 	trees := make([][]int, len(lines[0]))
 	for i, line := range lines {
@@ -55,7 +58,49 @@ func puzzle1(input string) {
 			trees[i][j] = height
 		}
 	}
+	return trees
+}
 
+func getScenicScore(i int, j int, trees [][]int) int {
+	tree := trees[i][j]
+	left_score := 0
+	right_score := 0
+	top_score := 0
+	bottom_score := 0
+
+	for a := i + 1; a < len(trees[i]); a++ {
+		right_score++
+		if trees[a][j] >= tree {
+			break
+		}
+	}
+
+	for b := i - 1; b >= 0; b-- {
+		left_score++
+		if trees[b][j] >= tree {
+			break
+		}
+	}
+
+	for c := j + 1; c < len(trees); c++ {
+		bottom_score++
+		if trees[i][c] >= tree {
+			break
+		}
+	}
+
+	for d := j - 1; d >= 0; d-- {
+		top_score++
+		if trees[i][d] >= tree {
+			break
+		}
+	}
+
+	return bottom_score * left_score * top_score * right_score
+}
+
+func puzzle1(input string) {
+	trees := parseTrees(input)
 	visible_count := 0
 	for i := 0; i < len(trees); i++ {
 		tree_line := trees[i]
@@ -69,7 +114,18 @@ func puzzle1(input string) {
 }
 
 func puzzle2(input string) {
-
+	trees := parseTrees(input)
+	max_score := 0
+	for i := 0; i < len(trees); i++ {
+		tree_line := trees[i]
+		for j := 0; j < len(tree_line); j++ {
+			score := getScenicScore(i, j, trees)
+			if score > max_score {
+				max_score = score
+			}
+		}
+	}
+	println(max_score)
 }
 
 func main() {
