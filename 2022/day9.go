beed8813fbebf095d285876c1a10cf6d30c1fe9a
Mackenzie Salloum <mackenziesalloum@gmail.com>
2022-12-09 19:00:00 -0800

    Condense a into single func

--- 2022/day9.go
+++ 2022/day9.go
@@ -17,58 +17,12 @@ func knotNeedsToMove(head *Position, tail *Position) bool {
 	return math.Abs(float64(head.x-tail.x)) > 1 || math.Abs(float64(head.y-tail.y)) > 1
 }
 
-func puzzle1(input string) {
-	instructions := strings.Split(input, "\n")
-	positions_occupied := make(map[string]bool)
-	head := Position{0, 0}
-	tail := Position{0, 0}
-	positions_occupied["0,0"] = true
-	for _, instruction_text := range instructions {
-		tokens := strings.Split(instruction_text, " ")
-		direction := tokens[0]
-		amount, _ := strconv.Atoi(tokens[1])
-		for i := 0; i < amount; i++ {
-			switch direction {
-			case "R":
-				head.x++
-			case "L":
-				head.x--
-			case "U":
-				head.y++
-			case "D":
-				head.y--
-			}
-			if knotNeedsToMove(&head, &tail) {
-				tail = calculateCatchUp(&head, &tail)
-				positions_occupied[fmt.Sprintf("%d,%d", tail.x, tail.y)] = true
-			}
-		}
-	}
-	println(len(positions_occupied))
-}
-
-func clamp(num, low, high int) int {
-	if num < low {
-		return low
-	}
-	if num > high {
-		return high
-	}
-	return num
-}
-
-func calculateCatchUp(head *Position, tail *Position) Position {
-	return Position{tail.x + clamp(head.x-tail.x, -1, 1), tail.y + clamp(head.y-tail.y, -1, 1)}
-}
-
-func puzzle2(input string) {
-	instructions := strings.Split(input, "\n")
-	positions_occupied := make(map[string]bool)
-	knots := [10]*Position{}
+func doPuzzle(instructions []string, knot_length int) {
+	positions_occupied := map[string]bool{"0,0": true}
+	knots := make([]*Position, knot_length)
 	for i := 0; i < len(knots); i++ {
 		knots[i] = &Position{}
 	}
-	positions_occupied["0,0"] = true
 	for _, instruction_text := range instructions {
 		tokens := strings.Split(instruction_text, " ")
 		direction := tokens[0]
@@ -89,17 +43,40 @@ func puzzle2(input string) {
 				current := knots[knot_index-1]
 				next := knots[knot_index]
 				if knotNeedsToMove(current, next) {
-					pos := calculateCatchUp(current, next)
-					knots[knot_index] = &pos
+					knots[knot_index] = calculateCatchUp(current, next)
 					if knot_index == len(knots)-1 {
 						positions_occupied[fmt.Sprintf("%d,%d", next.x, next.y)] = true
 					}
 				}
-
 			}
 		}
 	}
 	println(len(positions_occupied))
+}
+
+func clamp(num, low, high int) int {
+	if num < low {
+		return low
+	}
+	if num > high {
+		return high
+	}
+	return num
+}
+
+func calculateCatchUp(head *Position, tail *Position) *Position {
+	return &Position{tail.x + clamp(head.x-tail.x, -1, 1), tail.y + clamp(head.y-tail.y, -1, 1)}
+}
+
+func puzzle1(input string) {
+	instructions := strings.Split(input, "\n")
+	doPuzzle(instructions, 2)
+}
+
+func puzzle2(input string) {
+	instructions := strings.Split(input, "\n")
+	doPuzzle(instructions, 10)
+
 }
 
 func main() {
