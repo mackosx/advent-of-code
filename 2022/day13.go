c7f8eea2d92ae0fd800456fe0b9285d7c38aed10
Mackenzie Salloum <mackenziesalloum@gmail.com>
2022-12-29 22:04:10 -0600

    Complete day 13 part 2

--- 2022/day13.go
+++ 2022/day13.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 	"text/scanner"
@@ -18,7 +19,6 @@ func sumRange(start int, end int, arr []int) int {
 
 func ordered(left []any, right []any) (bool, bool) {
 	for i := 0; i < len(left) && i < len(right); i++ {
-		println(i, " ", len(left))
 		l := left[i]
 		r := right[i]
 		r_int, r_is_int := r.(int)
@@ -36,7 +36,6 @@ func ordered(left []any, right []any) (bool, bool) {
 				return is_ordered, false
 			}
 		} else if r_is_int && l_is_int {
-			println("Compare integers: ", l_int, " ", r_int)
 			if l_int < r_int {
 				// Make a decision; ordered
 				return true, false
@@ -74,7 +73,6 @@ func ordered(left []any, right []any) (bool, bool) {
 		return false, true
 	} else {
 		// Whichever list is smaller ran out first
-		fmt.Printf("Ran out of items: %v %v\n", left, right)
 		return len(left) < len(right), false
 	}
 }
@@ -98,7 +96,7 @@ func parseList(s *scanner.Scanner) []any {
 	return list
 }
 
-func isOrdered(pairs []string) bool {
+func isOrdered(pairs []string) (o bool) {
 	var s scanner.Scanner
 	s.Init(strings.NewReader(pairs[0]))
 	s.Scan()
@@ -106,10 +104,17 @@ func isOrdered(pairs []string) bool {
 	s.Init(strings.NewReader(pairs[1]))
 	s.Scan()
 	right := parseList(&s)
-	fmt.Printf("%v\n%v\n", left, right)
-	o, _ := ordered(left, right)
-	println(o)
-	return o
+	o, _ = ordered(left, right)
+	return
+}
+
+func findIndex(s []any, f func(any) bool) int {
+	for i, x := range s {
+		if f(x) {
+			return i
+		}
+	}
+	return -1
 }
 
 func puzzle1(input string) {
@@ -125,12 +130,32 @@ func puzzle1(input string) {
 }
 
 func puzzle2(input string) {
+	packet_text := strings.Split(input+"\n[[2]]\n[[6]]", "\n")
+	packets := []any{}
+	var s scanner.Scanner
+	for _, packet_text := range packet_text {
+		if packet_text == "" {
+			continue
+		}
+		s.Init(strings.NewReader(packet_text))
+		s.Scan()
+		l := parseList(&s)
+		packets = append(packets, l)
+	}
 
+	sort.Slice(packets, func(i, j int) bool {
+		ordered, _ := ordered(packets[i].([]any), packets[j].([]any))
+		return ordered
+	})
+	div1_index := findIndex(packets, func(packet any) bool { return fmt.Sprint(packet) == "[[2]]" }) + 1
+	div2_index := findIndex(packets, func(packet any) bool { return fmt.Sprint(packet) == "[[6]]" }) + 1
+
+	println(div1_index * div2_index)
 }
 
 func main() {
 	raw_input, _ := os.ReadFile("./day13_input.txt")
 	input := string(raw_input)
-	puzzle1(input)
-	// puzzle2(input)
+	// puzzle1(input)
+	puzzle2(input)
 }
